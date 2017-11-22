2e18ba2824e8cab172d569538cfa40177abc1df7
jasonduquain <jduquain@gmail.com>
2017-11-21 21:20:34 -0500

    final sects complete - MQs and JS next

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,108 +1,28 @@
-$(document).ready(function () {
-    
-    /* for the sticky nav */
-    
-    /*$('.js--section-features').waypoint(function (direction) { 
-        if (direction == "down") {
-            $('nav').addClass('sticky')
-        } else {
-            $('nav').removeClass('sticky')
-        }
-    }, {
-        offset: '60px;'
-    });*/
-    
-  
-    /* scroll on buttons */
-    
-    /*$('.js--scroll-to-plans').click(function () {
-        $('html, body') .animate({scrollTop: $('.js--section-plans').offset().top}, 1000);   
-    });
-    
-    $('.js--scroll-to-start').click(function () {
-        $('html, body') .animate({scrollTop: $('.js--section-features').offset().top}, 1000);   
-    });*/
-    
-    
-    /* Navigation scroll */
-    
-    /*$(function() {
-        $('a[href=#features],a[href=#works]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });*/
-    
-    
-    /* Animations on scroll */
-    
-    /*$('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
+/****EQUAL HEIGHT BLOCKQUOTES IN TESTIMONIAL SECT****/
+let eqHeights = document.querySelectorAll('.section-testimonials blockquote');
+let highest = 0;
 
-    $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    });
+for (let i = 0; i < eqHeights.length; i++) {
+    
+    if (eqHeights[i].scrollHeight > highest) {
+        highest = eqHeights[i].scrollHeight;
+    }
+}
 
-    $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
+for (let i = 0; i < eqHeights.length; i++) {
+    eqHeights[i].style.height = highest + 'px'; 
+}
 
-    $('.js--wp-4').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animated pulse');
-    }, {
-        offset: '50%'
-    });*/
-    
-    
-    /* Mobile navigation */
-    
-    /*$('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
+/*********UPDATE COPYRIGHT DATE**************/
+let year = document.querySelector('.year');
+let currentYear = new Date().getFullYear();
+year.textContent = currentYear;
 
-        nav.slideToggle(200);
 
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }        
-    });*/
-    
-    /* MAps */
-    
-    /*var map = new GMaps({
-        div: '.map',
-        lat: 34.163988,
-        lng: -84.196383,
-        zoom: 14
-        
-    });
-    
-    map.addMarker({
-        lat: 34.163988,
-        lng: -84.196383,
-        title: 'Home',
-        infoWindow: {
-            content: '<p>My house on the lake</p>'
-        }
-    });    */   
-      
-});                
+
+
+
+
+
+
+
